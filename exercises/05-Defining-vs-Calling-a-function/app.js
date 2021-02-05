ca078fcb1fc73966ce176ae8be88763f851a934e
toxigeno <melissaaraya@yahoo.com>
2021-02-05 20:52:06 +0000

    05 done

--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,10 @@
 //Define the function called "multi" that expects 2 parameters:
-
+    function multi(width,height)
+    {
+        return width * height;
+    }
+    var box = multi (5,4)
+    console.log(box)
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
