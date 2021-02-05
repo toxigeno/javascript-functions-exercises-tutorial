e178564ffd130ecc6f43c39e587aa149b0a7734e
toxigeno <melissaaraya@yahoo.com>
2021-02-05 17:44:47 +0000

    avance

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -4,6 +4,10 @@ function calculateArea(length,edge)
 }
 // Your code Below this line:
 
+var squareArea1 = calculateArea(4,4);
+var squareArea2 = calculateArea(2,2);
+var squareArea3 = calculateArea(5,5);
+
 
 
 
