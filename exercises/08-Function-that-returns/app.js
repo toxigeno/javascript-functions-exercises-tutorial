e77678af930d2c68dd697bfb3aed057245a70326
toxigeno <melissaaraya@yahoo.com>
2021-02-08 02:55:38 +0000

    esteban

--- exercises/08-Function-that-returns/app.js
+++ exercises/08-Function-that-returns/app.js
@@ -5,16 +5,7 @@ var euroToYen = function(euroValue){
 	return euroValue * 124.15;
 }
 
-var dollarToYen = function(dollarValue2){
-    return x * x;}
-
-
-//***** YOUR CODE BELOW ******///
-
 var valor1 = dollarToEuro (137,0.89);
-var valor2 = euroToYen (137,124.15)
-//var valor3 = dollarToYen ();
+var valor2 = euroToYen (121.93,124.15)
 
-console.log(valor1)
 console.log(valor2)
-//console.log(valor3)
