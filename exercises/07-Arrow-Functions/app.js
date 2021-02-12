f61e99eb4b786259d277f51bcc538e980688211f
toxigeno <melissaaraya@yahoo.com>
2021-02-12 00:46:11 +0000

    nada

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,7 +1,3 @@
-    function rapid = (str) => x * y;
-
-    var lower = rapid(toLowerCase)
-
-   // From this line up Do not change code below
+// From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
